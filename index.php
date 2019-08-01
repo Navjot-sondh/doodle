82adb9c89102394cfcfa84d73af31d71c873e0d2
Navjot <navjot.singh2472017@gmail.com>
2019-08-01 06:53:33 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Doodle</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    </head>
+    <body>
+    <div class="wrapper indexPage" >
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="assets/images/doodleLogo.png">
+        </div>
+        <div class="searchContainer">
+            <form action="search.php" method="get">
+
+            <input class="searchBox" name="term" type="test">
+            <input class="searchButton" type="submit" value="search">
+            </form>
+        </div>
+    </div>
+    </div>
+    </body>
+</html>
