eb8ef86e5f0554142b3176d4c05dac466f787c21
Navjot <navjot.singh2472017@gmail.com>
2019-08-07 19:58:17 +0530

    Search results complete

--- index.php
+++ index.php
@@ -1,23 +1,45 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Welcome to Doodle</title>
-        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    </head>
-    <body>
-    <div class="wrapper indexPage" >
-    <div class="mainSection">
-        <div class="logoContainer">
-            <img src="assets/images/doodleLogo.png">
-        </div>
-        <div class="searchContainer">
-            <form action="search.php" method="get">
+<head>
+	<title>Welcome to Doodle</title>
 
-            <input class="searchBox" name="term" type="test">
-            <input class="searchButton" type="submit" value="search">
-            </form>
-        </div>
-    </div>
-    </div>
-    </body>
+	<meta name="description" content="Search the web for sites and images.">
+	<meta name="keywords" content="Search engine, doodle, websites">
+	<meta name="author" content="Reece Kenney">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+
+	<div class="wrapper indexPage">
+	
+
+		<div class="mainSection">
+
+			<div class="logoContainer">
+				<img src="assets/images/doodleLogo.png" title="Logo of our site" alt="Site logo">
+			</div>
+
+
+			<div class="searchContainer">
+
+				<form action="search.php" method="GET">
+
+					<input class="searchBox" type="text" name="term">
+					<input class="searchButton" type="submit" value="Search">
+
+
+				</form>
+
+			</div>
+
+
+		</div>
+
+
+	</div>
+
+</body>
 </html>
